0d1aa078c265930770b7e5d2c526a0e0647b0623
zhxxch <zhxxch@outlook.com>
2015-08-27 14:21:51 +0800

    Update limregex.h

--- limregex.h
+++ limregex.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Xiaochen Z. <201560039.uibe.edu.cn>
+ * Copyright (c) 2015 X. ZHANG <201560039.uibe.edu.cn>
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated
